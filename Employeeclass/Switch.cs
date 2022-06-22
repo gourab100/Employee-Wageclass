000eabcb8aafa4c6463f229a5ae2d1bfde7c0fab
gourab <gourab.saheb100@gmail.com>
2022-06-22 19:25:20 +0530

    Calculate total

--- Employeeclass/Switch.cs
+++ Employeeclass/Switch.cs
@@ -10,32 +10,38 @@ namespace Employeeclass
     {
         public void Employee()
         {
-            
+
             int workinghours = 0;
             const int wagesperhours = 20;
             const int present = 2;
             const int parttime = 1;
+            const int workingdays = 2;
             int daliy_wage = 0;
-            Random random = new Random();
-            int employee = random.Next(0, 3);
-            switch (employee)
+            int totalempwage = 0;
+            for (int day = 0; day < workingdays; day++)
             {
-                case parttime:
-                    workinghours = 4;
-                    Console.WriteLine("parttimer");
-                    break;
-                case present:
-                    workinghours = 8;
-                    Console.WriteLine("present");
-                    break;
-                default:
-                    workinghours = 0;
-                    Console.WriteLine("absent");
-                    break;
+                Random random = new Random();
+                int employee = random.Next(0, 3);
+                switch (employee)
+                {
+                    case parttime:
+                        workinghours = 4;
+                        Console.WriteLine("parttimer");
+                        break;
+                    case present:
+                        workinghours = 8;
+                        Console.WriteLine("present");
+                        break;
+                    default:
+                        workinghours = 0;
+                        Console.WriteLine("absent");
+                        break;
+                }
+                daliy_wage = workinghours * wagesperhours;
+                totalempwage += daliy_wage;
+                Console.WriteLine("emp wage: " + daliy_wage);
             }
-            daliy_wage = workinghours * wagesperhours;
-            Console.WriteLine("emp wage: "+ daliy_wage);
-
+            Console.WriteLine("Total emp wage: " + totalempwage);
         }
     }
 }

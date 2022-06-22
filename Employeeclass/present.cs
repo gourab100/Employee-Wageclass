c617aebd928737cd117ba02297992ef518c3eefa
gourab <gourab.saheb100@gmail.com>
2022-06-22 18:41:45 +0530

    Add: partimer

--- Employeeclass/present.cs
+++ Employeeclass/present.cs
@@ -10,28 +10,35 @@ namespace Employeeclass
     {
         public void main()
         {
-            int fulldayperhours = 0;
+            int fulldayperhours = 8;
+            int workinghours = 0;
             int wagesperhours = 20;
-            int present = 1;
+            int present = 2;
+            int parttime = 1;
             int daliy_wage = 0;
             Random random = new Random();
-            int employee = random.Next(0, 2);
+            int employee = random.Next(0, 3);
 
-            if (employee == present)
+            if (employee == parttime)
             {
-                fulldayperhours = 8;
+                workinghours = workinghours + fulldayperhours;
+                Console.WriteLine("parttimer");
+
+
+            }
+            else if (employee == present)
+            {
+                workinghours = (fulldayperhours - workinghours) / 2;
                 Console.WriteLine("present");
 
             }
             else
             {
-                fulldayperhours = 0;
                 Console.WriteLine("absent");
             }
             daliy_wage = fulldayperhours * wagesperhours;
             Console.WriteLine("daliy employee wage :" + daliy_wage);
-        }
 
-       
+        }
     }
 }

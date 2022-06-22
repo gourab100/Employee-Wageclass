b0b6f604a3b999018cb4a16e226d96152f6c0527
gourab <gourab.saheb100@gmail.com>
2022-06-22 18:37:34 +0530

    Calculate Daliywage

--- Employeeclass/present.cs
+++ Employeeclass/present.cs
@@ -10,22 +10,26 @@ namespace Employeeclass
     {
         public void main()
         {
+            int fulldayperhours = 0;
+            int wagesperhours = 20;
             int present = 1;
-            //int daliy_wage = 0;
+            int daliy_wage = 0;
             Random random = new Random();
             int employee = random.Next(0, 2);
 
             if (employee == present)
             {
-                //  fulldayperhours = 8;
+                fulldayperhours = 8;
                 Console.WriteLine("present");
 
             }
             else
             {
-                //fulldayperhours = 0;
+                fulldayperhours = 0;
                 Console.WriteLine("absent");
             }
+            daliy_wage = fulldayperhours * wagesperhours;
+            Console.WriteLine("daliy employee wage :" + daliy_wage);
         }
 
        
